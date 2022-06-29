faad6628c790542c4c9d2ce46d693cd0528b7000
Sara Heath <saramaheath@gmail.com>
2022-06-29 14:27:51 -0500

    added mean median and mode routes to app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+const { response } = require("express");
 /** Simple demo Express app. */
 
 const express = require("express");
@@ -5,18 +7,54 @@ const app = express();
 
 // useful error class to throw
 const { NotFoundError } = require("./expressError");
+const { findMean, findMedian, findMode } = require("./stats");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
+app.use(express.json());                           // process JSON data
+app.use(express.urlencoded({ extended: true }));   // process trad form data
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-
+app.get('/mean', function(req, res) {
+  
+  const strNums = req.query.nums.split(',');
+  const intNums = strNums.map(n => parseInt(n));
+  const value = findMean(intNums);
+  const response = {
+    operation: 'mean',
+    value: value
+  };
+  
+  return res.send(response); 
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
-
+app.get('/median', function(req, res) {
+  
+  const strNums = req.query.nums.split(',');
+  const intNums = strNums.map(n => parseInt(n));
+  const value = findMedian(intNums);
+  const response = {
+    operation: 'median',
+    value: value
+  };
+  
+  return res.send(response); 
+});
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
-
+app.get('/mode', function(req, res) {
+  
+  const strNums = req.query.nums.split(',');
+  const intNums = strNums.map(n => parseInt(n));
+  const value = findMode(intNums);
+  const response = {
+    operation: 'mode',
+    value: value
+  };
+  
+  return res.send(response); 
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
